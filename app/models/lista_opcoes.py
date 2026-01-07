319de7a0f35988d34bdefb4bcbdaf7fa787aaf87
werneckx <er4273270@gmail.com>
2026-01-07 10:35:48 -0300

    Troca de clima para lista_opcoes

---
+++ app/models/lista_opcoes.py
@@ -0,0 +1,21 @@
+from app import db
+
+class ListaOpcaoBase(db.Model):
+    __tablename__ = "lista_opcoes"
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(120), nullable=False)
+    tipo_lista = db.Column(db.String(50), nullable=False)
+
+    __mapper_args__ = {
+        'polymorphic_on': tipo_lista,
+    }
+
+class Clima(ListaOpcaoBase):
+    __mapper_args__ = {
+        'polymorphic_identity': 'Climas',
+    }
+
+class Equipamento(ListaOpcaoBase):
+    __mapper_args__ = {
+        'polymorphic_identity': 'Equipamentos',
+    }
